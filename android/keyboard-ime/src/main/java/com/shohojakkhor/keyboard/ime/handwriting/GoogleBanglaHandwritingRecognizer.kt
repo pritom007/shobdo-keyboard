dfbd90875eb820038be4b079a6a0c9fdc1a7be22
pritom007 <pr428@aifi.com>
2026-08-05 09:32:06 +0600

    Use ML Kit 18 digital ink package

--- android/keyboard-ime/src/main/java/com/shohojakkhor/keyboard/ime/handwriting/GoogleBanglaHandwritingRecognizer.kt
+++ android/keyboard-ime/src/main/java/com/shohojakkhor/keyboard/ime/handwriting/GoogleBanglaHandwritingRecognizer.kt
@@ -2,12 +2,12 @@ package com.shohojakkhor.keyboard.ime.handwriting
 
 import com.google.mlkit.common.model.DownloadConditions
 import com.google.mlkit.common.model.RemoteModelManager
-import com.google.mlkit.vision.digitalink.recognition.DigitalInkRecognition
-import com.google.mlkit.vision.digitalink.recognition.DigitalInkRecognitionModel
-import com.google.mlkit.vision.digitalink.recognition.DigitalInkRecognitionModelIdentifier
-import com.google.mlkit.vision.digitalink.recognition.DigitalInkRecognizer
-import com.google.mlkit.vision.digitalink.recognition.DigitalInkRecognizerOptions
-import com.google.mlkit.vision.digitalink.recognition.Ink
+import com.google.mlkit.vision.digitalink.DigitalInkRecognition
+import com.google.mlkit.vision.digitalink.DigitalInkRecognitionModel
+import com.google.mlkit.vision.digitalink.DigitalInkRecognitionModelIdentifier
+import com.google.mlkit.vision.digitalink.DigitalInkRecognizer
+import com.google.mlkit.vision.digitalink.DigitalInkRecognizerOptions
+import com.google.mlkit.vision.digitalink.Ink
 
 /** Google ML Kit `bn` recognizer. Stroke content never leaves the device. */
 public class GoogleBanglaHandwritingRecognizer : BanglaHandwritingRecognizer {
